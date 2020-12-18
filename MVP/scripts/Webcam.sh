43d4d7c10202c5351c6028f51aa0e2a9cb923f23
Doug Anderson <37873442+jdanderson54@users.noreply.github.com>
2020-12-18 12:54:19 -0600

    Add files via upload

--- MVP/scripts/Webcam.sh
+++ MVP/scripts/Webcam.sh
@@ -2,4 +2,6 @@
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-fswebcam -r 1280x720 --no-banner /home/pi/MVP/pictures/$DATE.jpg
+#fswebcam -r 1280x720 --no-banner /home/pi/MVP/pictures/$DATE.jpg
+
+fswebcam -r 1280x720 --title Sammies_MARSfarm /home/pi/MVP/pictures/$DATE.jpg
